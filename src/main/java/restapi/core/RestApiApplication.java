96b48ee329b04ab015a06202e41421c7454f41ea
devisv505 <devisv505@gmail.com>
2020-06-09 23:02:19 +0300

    cleaned extra code

--- src/main/java/restapi/core/RestApiApplication.java
+++ src/main/java/restapi/core/RestApiApplication.java
@@ -2,18 +2,15 @@ package restapi.core;
 
 import restapi.core.config.Config;
 import restapi.core.config.JavaConfig;
-import restapi.core.server.JettyServer;
 
-public class RestapiApplication {
+public class RestApiApplication {
 
   public static ApplicationContext run(Class mainClass) {
     Config javaConfig = new JavaConfig(mainClass.getPackageName());
     ApplicationContext context = new ApplicationContext(javaConfig);
     ObjectFactory objectFactory = new ObjectFactory(context);
-    context.setObjectFactory(objectFactory);
 
-    JettyServer server = context.getObject(JettyServer.class);
-    server.start();
+    context.setObjectFactory(objectFactory);
 
     return context;
   }
--- src/main/java/restapi/core/proxy/DeprecatedHandlerProxyConfigurator.java
+++ src/main/java/restapi/core/proxy/DeprecatedHandlerProxyConfigurator.java
@@ -6,12 +6,8 @@ import java.lang.reflect.Proxy;
 import java.util.Arrays;
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.InvocationHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class DeprecatedHandlerProxyConfigurator implements ProxyConfigurator {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger("DeprecatedHandlerProxy");
 
   @Override
   public Object wrapInProxyIfNeeded(Object t, Class implClass) {
@@ -33,13 +29,13 @@ public class DeprecatedHandlerProxyConfigurator implements ProxyConfigurator {
   }
 
   private Object getInvocationHandler(Object t, Method method, Object[] args) throws IllegalAccessException, InvocationTargetException {
-    LOGGER.warn("WARNING: class " + t.getClass().getName() + " is deprecated.");
+    System.out.println("WARNING: class " + t.getClass().getName() + " is deprecated.");
     return method.invoke(t, args);
   }
 
   private Object getInvocationHandlerMethod(Object t, Method method, Object[] args) throws IllegalAccessException, InvocationTargetException {
     if (method.isAnnotationPresent(Deprecated.class)) {
-      LOGGER.warn("WARNING: Method " + t.getClass().getName() + "#" + method.getName() + " is deprecated.");
+      System.out.println("WARNING: Method " + t.getClass().getName() + "#" + method.getName() + " is deprecated.");
     }
 
     return method.invoke(t, args);
